3f6a521c9301a4dcdac6d3f77e0ea8dd0d511653
Peter Darrow <pmdarrow@gmail.com>
2018-04-02 01:54:55 -0300

    Add Github link to main template

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,7 @@
     </section>
     <footer class="info">
       <p>Double-click to edit a todo</p>
-      <p>Written by <a href="http://evanyou.me">Peter Darrow</a></p>
+      <p>Written by <a href="https://github.com/pmdarrow">Peter Darrow</a></p>
       <p>Based on <a href="https://github.com/vuejs/vue/tree/dev/examples/todomvc">Vue.js TodoMVC</a></p>
     </footer>
   </div>
