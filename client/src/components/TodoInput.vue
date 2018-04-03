4300731901c5ce1526e393e163c3723f6c84dc7c
Peter Darrow <pmdarrow@gmail.com>
2018-04-03 01:04:53 -0300

    Remove toggle all complete checkbox

--- client/src/components/TodoInput.vue
+++ client/src/components/TodoInput.vue
@@ -1,8 +1,5 @@
 <template>
   <header class="header">
-    <label class="toggle-all-complete">
-      <input type="checkbox" />
-    </label>
     <input
       class="new-todo"
       autofocus
@@ -42,16 +39,5 @@ export default {
 .header {
   display: flex;
   border-top: 1px solid #ededed;
-}
-.toggle-all-complete {
-  display: flex;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.003);
-  box-shadow: inset 1px -2px 1px rgba(0, 0, 0, 0.03);
-  padding: 0 15px;
-}
-.new-todo {
-  padding-left: 11px;
-  box-shadow: inset -1px -2px 1px rgba(0, 0, 0, 0.03);
 }
 </style>
